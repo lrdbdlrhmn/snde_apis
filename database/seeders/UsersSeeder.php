a9bb4b9b038a08edae0adcb3b63d80499f57a6be
rsn <lrdbdlrhmn@gmail.com>
2022-09-27 13:42:27 +0200

    version 1.2

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UsersSeeder extends Seeder
 {
@@ -24,6 +27,7 @@ class UsersSeeder extends Seeder
             "user_type" => "user",
             "email" => "ali_m@gmail.com",
             "whatsapp" => "45679015",
+            "password" => Hash::make('1234'),
             "city_id" => 1,
             "state_id" => 1,
             "region_id" => 1
