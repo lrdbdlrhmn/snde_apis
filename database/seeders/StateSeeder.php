12525f2aacbf99f2da8edb85980a410bcb1fbc2b
rsn <lrdbdlrhmn@gmail.com>
2022-09-27 13:50:36 +0200

    version 1.2

--- database/seeders/StateSeeder.php
+++ database/seeders/StateSeeder.php
@@ -4,7 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 class StateSeeder extends Seeder
 {
     /**
