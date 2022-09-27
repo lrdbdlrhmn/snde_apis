d6460ff8ecc070cbcd6e5acaa75314a521f51096
rsn <lrdbdlrhmn@gmail.com>
2022-09-27 13:55:53 +0200

    version 1.2

--- database/seeders/RegionSeeder.php
+++ database/seeders/RegionSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class RegionSeeder extends Seeder
 {
@@ -14,6 +16,13 @@ class RegionSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('regions')->insert([
+            "name" => "Ilpe Adresse",
+            "name_fr" => "Ilpe Adresse",
+            "description" => "Ilpe Adresse",
+            "status" => 1,
+            "city_id" => 1
+        ]);
         //
     }
 }
